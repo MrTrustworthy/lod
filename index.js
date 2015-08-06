f072c29ae680a361aa40d1a020a627bd330b3f2c
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2015-08-06 23:20:28 +0200

    deploy test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, its working!\n');
+  res.end('Hello, its really! working!\n');
 }).listen(61768, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:61768/');
 return;
